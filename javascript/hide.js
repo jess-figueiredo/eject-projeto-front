ce8d10161b29bdcfe72f77af38fa9dc05f9a5b5d
Jéssica de Figueiredo <50491288+jess-figueiredo@users.noreply.github.com>
2022-10-20 15:36:55 -0300

    first commit

---
+++ javascript/hide.js
@@ -0,0 +1,5 @@
+var hide = document.querySelector(".icone-drop")
+hide.addEventListener("click", function(){
+    var navbar__wrap = document.querySelector("#nav-aside");
+    navbar__wrap.classList.toggle("hide")
+})
